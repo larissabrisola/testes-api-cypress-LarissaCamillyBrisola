d509081f94608de9354e0c224ff4044a95cb3193
larissabrisola <alarissabrisola@gmail.com>
2024-04-16 16:10:57 -0300

    testes de login ok

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -86,27 +86,30 @@ describe('Cadastro de usuário', () => {
             method: 'POST',
             url: `${baseUrl}/users`,
             body: {
-                "name": "Juca Fernandes",
+                "name": "Luis",
                 "email": "juquinha@gmail.com",
                 "password": "lwalala"
             }, failOnStatusCode: false
 
-        }).then((response)=>{
+        }).then((response) => {
             expect(response.status).to.equal(409);
             expect(response.body).to.deep.equal(
                 {
-                "message": "Email already in use",
-                "error": "Conflict",
-                "statusCode": 409
+                    "message": "Email already in use",
+                    "error": "Conflict",
+                    "statusCode": 409
                 })
 
 
         })
 
+        // add teste de campos vazios 
 
     })
 
 })
+
+
 
 describe('Login', () => {
 
@@ -115,18 +118,124 @@ describe('Login', () => {
     })
 
     it('Login realizado com sucesso', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl,
+            body: {
+                email: "juquinha@gmail.com",
+                password: "lwalala"
+            }
+        }).its('status').should('to.equal', 200)
     })
 
     it('Login não realizado - senha incorreta', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl,
+            body: {
+                email: "juquinha@gmail.com",
+                password: "oioiwoi"
+            }, failOnStatusCode: false
 
+        }).then((response) => {
+
+            expect(response.status).to.equal(401)
+            expect(response.body).to.deep.equal({
+                "message": "Invalid username or password.",
+                "error": "Unauthorized",
+                "statusCode": 401
+            })
+        })
     })
 
     it('Login não realizado - email não cadastrado', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl,
+            body: {
+                email: "jbigfanavrillavigne2013@gmail.com",
+                password: "oioiwoi"
+            }, failOnStatusCode: false
 
+        }).then((response) => {
+
+            expect(response.status).to.equal(401)
+            expect(response.body).to.deep.equal({
+                "message": "Invalid username or password.",
+                "error": "Unauthorized",
+                "statusCode": 401
+            })
+
+        })
     })
 
     it('Login não realizado - formato de email inválido', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl,
+            body: {
+                email: "jojocadom",
+                password: "1234567j"
+            }, failOnStatusCode: false
 
+        }).then((response) => {
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.deep.equal({
+                "message": [
+                    "email must be an email"
+                ],
+                "error": "Bad Request",
+                "statusCode": 400
+            })
+        })
+    })
+
+
+    it('login não realizado - campo senha vazio', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl,
+            body: {
+                email: "juquinha@gmail.com",
+                password: ""
+            }, failOnStatusCode: false
+
+        }).then((response) => {
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.deep.equal({
+                "message": [
+                    "password should not be empty"
+                ],
+                "error": "Bad Request",
+                "statusCode": 400
+            })
+        })
+    })
+
+    it('login não realizado - campo email vazio', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl,
+            body: {
+                email: "",
+                password: "aaaaaaaaw"
+            }, failOnStatusCode: false
+
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.deep.equal({
+                "message": [
+                    "email should not be empty",
+                    "email must be an email"
+                ],
+                "error": "Bad Request",
+                "statusCode": 400
+            }
+
+            )
+        })
     })
 
 })
