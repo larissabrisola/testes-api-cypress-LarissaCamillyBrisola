364cefceb356a8d36f98959f625e85f946044c29
larissabrisola <alarissabrisola@gmail.com>
2024-04-16 15:37:32 -0300

    testes de cadastro ok

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,83 +1,133 @@
 
 import { faker } from '@faker-js/faker';
-        
 
-describe('Cadastro de usuário', ()=>{
+let baseUrl;
 
-    let baseUrl;
+describe('Cadastro de usuário', () => {
     let randomName;
-    let randomEmail; 
-    
-    beforeEach(()=>{
-         baseUrl = 'https://raromdb-3c39614e42d4.herokuapp.com/api'
+    let randomEmail;
 
-         randomName = faker.person.fullName(); // Rowan Nikolaus
-         randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+    beforeEach(() => {
+        baseUrl = 'https://raromdb-3c39614e42d4.herokuapp.com/api'
+
+        randomName = faker.person.fullName();
+        randomEmail = faker.internet.email();
     })
 
-    it('Cadastro realizado com sucesso', ()=>{
-         cy.request({
-            method: 'POST', 
+    it('Cadastro realizado com sucesso', () => {
+
+        cy.log(randomEmail)
+        cy.request({
+            method: 'POST',
             url: `${baseUrl}/users`,
             body: {
                 "name": randomName,
                 "email": randomEmail,
                 "password": "lwalala"
-              }
+            }
 
-         }).its('status').should('to.equal', 201)
+        }).its('status').should('to.equal', 201)
 
 
     })
 
-    it('Cadastro não realizado - senha curta demais (deve ter de 6 a 12 caracteres)', ()=>{
+    it('Cadastro não realizado - senha curta demais (minimo 6 caracteres)', () => {
+
         cy.request({
-            method: 'POST', 
+            method: 'POST',
             url: `${baseUrl}/users`,
             body: {
                 "name": randomName,
                 "email": randomEmail,
                 "password": "lwa"
-              }, failOnStatusCode: false
+            }, failOnStatusCode: false
 
-         }).its('status').should('to.equal', 400)
+        }).then((response) => {
+
+            expect(response.status).to.equal(400);
+            expect(response.body).to.deep.equal({
+                "message": [
+                    "password must be longer than or equal to 6 characters"
+                ],
+                "error": "Bad Request",
+                "statusCode": 400
+            })
 
 
-         
+        })
+
     })
 
-    it('Cadastro não realizado - email já cadastrado ', ()=>{
-        
+    it('Cadastro não realizado - formato de email inválido', () => {
+        cy.request({
+            method: 'POST',
+            url: `${baseUrl}/users`,
+            body: {
+                "name": randomName,
+                "email": "jojocacom",
+                "password": "lwalala"
+            }, failOnStatusCode: false
+
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.deep.equal({
+                "message": [
+                    "email must be an email"
+                ],
+                "error": "Bad Request",
+                "statusCode": 400
+            })
+
+        })
     })
 
-    it('Cadastro não realizado - formato de email inválido', ()=>{
-        
+    it('Cadastro não realizado - email já cadastrado ', () => {
+        cy.request({
+            method: 'POST',
+            url: `${baseUrl}/users`,
+            body: {
+                "name": "Juca Fernandes",
+                "email": "juquinha@gmail.com",
+                "password": "lwalala"
+            }, failOnStatusCode: false
+
+        }).then((response)=>{
+            expect(response.status).to.equal(409);
+            expect(response.body).to.deep.equal(
+                {
+                "message": "Email already in use",
+                "error": "Conflict",
+                "statusCode": 409
+                })
+
+
+        })
+
+
     })
+
 })
 
-describe('Login', ()=>{
+describe('Login', () => {
 
-    it('Login realizado com sucesso', ()=>{
+    beforeEach(() => {
+        baseUrl = 'https://raromdb-3c39614e42d4.herokuapp.com/api/auth/login'
+    })
+
+    it('Login realizado com sucesso', () => {
+    })
+
+    it('Login não realizado - senha incorreta', () => {
 
     })
 
-    it('Login não realizado - senha incorreta', ()=>{
+    it('Login não realizado - email não cadastrado', () => {
 
     })
 
-    it('Login não realizado - email não cadastrado', ()=>{
-
-    })
-
-    it('Login não realizado - formato de email inválido', ()=>{
+    it('Login não realizado - formato de email inválido', () => {
 
     })
 
 })
 
-describe('Consultar usuário', ()=>{
-
-    it('', ()=>{
-
-    })
-})
