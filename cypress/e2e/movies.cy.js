b99a6e422a08fa04c347c0997b782109d4c64a61
larissabrisola <alarissabrisola@gmail.com>
2024-04-22 14:22:46 -0300

    add casos de testes em cadastro / other stuff

--- cypress/e2e/movies.cy.js
+++ cypress/e2e/movies.cy.js
@@ -4,8 +4,33 @@ let token
 //admin funcs
 describe('Cadastro de filmes', () => {
 
-    // usar cy.commands aqui
-    it('Cadastro filme - sucesso', () => {
+    it('Cadastro filme - sucesso', function ()  {
+        cy.fixture('cadastroFilme').as('cadastroFilme')
+
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            // procurar api para randomizar
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: this.cadastroFilme,
+                headers: {
+                    Authorization: `${token}`
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(201);
+                expect(response.body).to.have.property('id');
+                expect(response.body).to.have.property('title')
+                expect(response.body).to.have.property('description')
+                expect(response.body).to.have.property('durationInMinutes')
+                expect(response.body).to.have.property('releaseYear')
+                expect(response.body).to.have.property('genre')
+
+            })
+        })
+    })
+
+    it('Cadastro filme - bad request - title vazio', ()=>{
         cy.perfilAdm(true).then((response) => {
             token = response.requestHeaders.Authorization
             // procurar api para randomizar
@@ -13,7 +38,7 @@ describe('Cadastro de filmes', () => {
                 method: 'POST',
                 url: `/movies`,
                 body: {
-                    "title": "xmen",
+                    "title": "",
                     "genre": "desenho",
                     "description": "xmen",
                     "durationInMinutes": 120,
@@ -21,60 +46,341 @@ describe('Cadastro de filmes', () => {
                 },
                 headers: {
                     Authorization: `${token}`
-                }
+                }, failOnStatusCode: false
             }).then((response) => {
-                expect(response.status).to.equal(201);
-                cy.log(response)
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal( {
+                    "message": [
+                    "title must be longer than or equal to 1 characters",
+                    "title should not be empty"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
 
             })
         })
     })
+    it('Cadastro filme - bad request - genre vazio', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "xuxa",
+                    "genre": "",
+                    "description": "xmen",
+                    "durationInMinutes": 120,
+                    "releaseYear": 2007
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal( {
+                    "message": [
+                    "genre must be longer than or equal to 1 characters",
+                    "genre should not be empty"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
 
-    it('Cadastro filme - bad request - title vazio')
-    it('Cadastro filme - bad request - genre vazio')
-    it('Cadastro filme - bad request - description vazio')
-    it('Cadastro filme - bad request - durationMinutes vazio')
-    it('Cadastro filme - bad request - releaseYear vazio')
+            })
+        })
+    })
+    it('Cadastro filme - bad request - description vazio', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "lua de cristal",
+                    "genre": "desenho",
+                    "description": "",
+                    "durationInMinutes": 120,
+                    "releaseYear": 2007
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal( {
+                    "message": [
+                    "description must be longer than or equal to 1 characters",
+                    "description should not be empty"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
 
+            })
+        })
+    })
+    it('Cadastro filme - bad request - durationMinutes vazio', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "Bola",
+                    "genre": "desenho",
+                    "description": "xmen",
+                    "durationInMinutes": '',
+                    "releaseYear": 2007
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal(
+                    {
+                        "message": [
+                        "durationInMinutes must be a number conforming to the specified constraints",
+                        "durationInMinutes should not be empty"
+                        ],
+                        "error": "Bad Request",
+                        "statusCode": 400
+                        }
+                )
+            })
+        })
+    })
+    it('Cadastro filme - bad request - releaseYear vazio', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "Matrix",
+                    "genre": "desenho",
+                    "description": "xmen",
+                    "durationInMinutes": 120,
+                    "releaseYear": ''
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal( {
+                    "message": [
+                    "releaseYear must be a number conforming to the specified constraints",
+                    "releaseYear should not be empty"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
+
+            })
+        })
+    })
+    it('Cadastro filme - bad request - todos os campos vazios', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "",
+                    "genre": "",
+                    "description": "",
+                    "durationInMinutes": '',
+                    "releaseYear": ''
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal( {
+                    "message": [
+                    "title must be longer than or equal to 1 characters",
+                    "title should not be empty",
+                    "genre must be longer than or equal to 1 characters",
+                    "genre should not be empty",
+                    "description must be longer than or equal to 1 characters",
+                    "description should not be empty",
+                    "durationInMinutes must be a number conforming to the specified constraints",
+                    "durationInMinutes should not be empty",
+                    "releaseYear must be a number conforming to the specified constraints",
+                    "releaseYear should not be empty"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
+
+            })
+        })
+    })
     // 
 
-    it('Cadastro filme - bad request - title formato inválido')
-    it('Cadastro filme - bad request - genre formato inválido')
-    it('Cadastro filme - bad request - description formato inválido')
-    it('Cadastro filme - bad request - durationMinutes formato inválido')
-    it('Cadastro filme - bad request - releaseYear formato inválido')
+    it('Cadastro filme - bad request - title formato inválido', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            // procurar api para randomizar
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": 123,
+                    "genre": "desenho",
+                    "description": "xmen",
+                    "durationInMinutes": 120,
+                    "releaseYear": 2007
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal({
+                    "message": [
+                    "title must be longer than or equal to 1 and shorter than or equal to 100 characters",
+                    "title must be a string"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
 
+            })
+        })
+    })
+    it('Cadastro filme - bad request - genre formato inválido', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            // procurar api para randomizar
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "moranguinho",
+                    "genre": 1243,
+                    "description": "xmen",
+                    "durationInMinutes": 120,
+                    "releaseYear": 2007
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal(  {
+                    "message": [
+                    "genre must be longer than or equal to 1 and shorter than or equal to 100 characters",
+                    "genre must be a string"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
 
+            })
+        })
+    })
+    it('Cadastro filme - bad request - description formato inválido', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            // procurar api para randomizar
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "Sailor moon",
+                    "genre": "desenho",
+                    "description": 123,
+                    "durationInMinutes": 120,
+                    "releaseYear": 2007
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal(  {
+                    "message": [
+                    "description must be longer than or equal to 1 and shorter than or equal to 500 characters",
+                    "description must be a string"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
 
+            })
+        })
+    })
+    it('Cadastro filme - bad request - durationMinutes formato inválido', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            // procurar api para randomizar
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "Sailor moon",
+                    "genre": "desenho",
+                    "description": "oiiiiiiiiiiiii",
+                    "durationInMinutes": '120',
+                    "releaseYear": 2007
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal( {
+                    "message": [
+                    "durationInMinutes must be a number conforming to the specified constraints"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
+
+            })
+        })
+    })
+    it('Cadastro filme - bad request - releaseYear formato inválido', ()=>{
+        cy.perfilAdm(true).then((response) => {
+            token = response.requestHeaders.Authorization
+            // procurar api para randomizar
+            cy.request({
+                method: 'POST',
+                url: `/movies`,
+                body: {
+                    "title": "Sailor moon",
+                    "genre": "desenho",
+                    "description": "lorem ipsum dolor sit amet",
+                    "durationInMinutes": 120,
+                    "releaseYear": '2007'
+                },
+                headers: {
+                    Authorization: `${token}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body).to.deep.equal( {
+                    "message": [
+                    "releaseYear must be a number conforming to the specified constraints"
+                    ],
+                    "error": "Bad Request",
+                    "statusCode": 400
+                    })
+
+            })
+        })
+    })
 })
 
 //admin funcs
 describe('Atualização de filmes', () => {
-// DEPENDENTE EMOCIONAL DA INTEGRIDADE DOS DADOS DA API (SOZINHO NAO CHEGA EM LUGAR NENHUM)
-    // aguardando a sessão da terapia (resposta dos instrutores)
-    beforeEach(() => {
-        cy.fixture('updateMovie.json').as('updateMovie');
-    })
-    it('Atualizar toda informação do filme com sucesso', function()  {
 
-        cy.log(this.updateMovie);
-        cy.perfilAdm(true).then((response) => {
-            token = response.requestHeaders.Authorization
-            // procurar api para   
-            cy.request({
-                method: 'PUT',
-                url: `/movies/13`,
-                body: this.updateMovie,
-                headers: {
-                    Authorization: `${token}`
-                }
-            }).then((response) => {
-                expect(response.status).to.equal(204);
-                cy.log(response.requestBody)
-
-            })
-        })
-    })
 })
 
 
@@ -94,7 +400,7 @@ describe('Consultar filmes', () => {
 
         cy.request({
             method: 'GET',
-            url: `/movies/19`
+            url: `/movies/22`
 
         }).then((response) => {
             expect(response.status).to.equal(200)
@@ -107,8 +413,6 @@ describe('Consultar filmes', () => {
             expect(response.body).to.have.property('durationInMinutes')
             expect(response.body).to.have.property('releaseYear')
             expect(response.body).to.have.property('reviews')
-
-
         })
 
     })
